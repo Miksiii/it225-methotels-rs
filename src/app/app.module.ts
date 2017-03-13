c27f023fae504fd6482610b3585cb408ce6d3449
milanhakajprodakshn@gmail.com <milanhakajprodakshn@gmail.com>
2017-03-13 16:52:47 +0100

    it225 - z09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
